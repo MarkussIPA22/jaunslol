2c11605dad25bcd473fa48ae24c80c2d063b43f1
MarkussIPA22 <125544038+MarkussIPA22@users.noreply.github.com>
2023-10-10 15:26:14 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+[{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 2,
+  "id": 2,
+  "title": "et porro tempora",
+  "completed": true
+},
+{
+  "userId": 6,
+  "id": 3,
+  "title": "fugiat veniam minus",
+  "completed": false
+},
+{
+  "userId": 31,
+  "id": 4,
+  "title": "quis ut nam facilis et officia qui",
+  "completed": true
+},
+{
+  "userId": 12,
+  "id": 5,
+  "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+  "completed": false
+},
+{
+  "userId": 85,
+  "id": 6,
+  "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+  "completed": false
+}]
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
